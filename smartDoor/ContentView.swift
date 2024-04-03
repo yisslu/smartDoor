7721a146159faf4d6db26d1eb42f80abeb2b4d4f
yisslu <lugohalo999@gmail.com>
2024-04-02 19:54:23 -0600

    Initial Commit

---
+++ smartDoor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  smartDoor
+//
+//  Created by Jesús Lugo Sáenz on 02/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ smartDoor/smartDoorApp.swift
@@ -0,0 +1,17 @@
+//
+//  smartDoorApp.swift
+//  smartDoor
+//
+//  Created by Jesús Lugo Sáenz on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct smartDoorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
